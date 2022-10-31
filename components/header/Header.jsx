fd0066fb56071c6505e7e5d2a555e8c464a73a79
Sergei Johansen <sergeitheprogrammer@gmail.com>
2022-10-31 15:48:14 +0100

    dd

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -70,7 +70,7 @@ function Header() {
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
-                        transition={{ duration: 0.5, delay: 0.2 }}
+                        transition={{ duration: 0.5 }}
                     >
                         <SecondaryNav />
                         <div className={styles.icons}>
